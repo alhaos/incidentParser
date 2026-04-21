05db6e4882197ad63c8dfb27c10cd66b75ca8a68
ALSemenov <alhaos@gmail.com>
2026-04-21 08:26:14 +0500

    log messages added

--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log/slog"
 	"os"
 	"parser/internal/config"
@@ -11,27 +10,30 @@ import (
 	"parser/internal/model"
 	"parser/internal/reporter"
 	"path/filepath"
+	"strings"
 )
 
 const incidentsPath = `data/Incidents`
 const templatePath = `template/template.html`
 const defaultConfigPath = `config/config.yml`
+const fileExtension = ".txt"
 
 func main() {
 
+	// Parse config filename flag
 	configFilenamePointer := flag.String("config", defaultConfigPath, "-config config/config.yml")
 	flag.Parse()
 	configFilename := *configFilenamePointer
 
-	fmt.Println(os.Getwd())
-
+	// Init configuration
 	conf, err := config.NewConfig(configFilename)
 	if err != nil {
 		panic(err)
 	}
 
+	// init log
 	l := slog.New(
-		slog.NewJSONHandler(
+		slog.NewTextHandler(
 			os.Stdout,
 			nil,
 		),
@@ -39,46 +41,66 @@ func main() {
 
 	slog.SetDefault(l)
 
+	// Get data files list
 	files, err := os.ReadDir(incidentsPath)
 
 	if err != nil {
 		panic(err)
 	}
 
+	// Init parser
 	p := incidentParser.NewParser()
 
+	// Init interpreter
 	i := interpreter.Default()
 
 	var importantIncidents []model.Incident
+	var counter int
 
-	for _, file := range files {
+	// Loop files
+	for _, entry := range files {
 
-		incident, err := p.Parse(filepath.Join(incidentsPath, file.Name()))
+		name := entry.Name()
+
+		if entry.IsDir() {
+			slog.Debug("found: ", "name", name, "skip reason", "is dir")
+			continue
+		}
+
+		counter++
+
+		ext := filepath.Ext(name)
+		lowerExt := strings.ToLower(ext)
+		if lowerExt != strings.ToLower(fileExtension) {
+			slog.Debug("found: ", "name", name, "skip reason", "Incorrect file extension")
+			continue
+		}
+
+		incident, err := p.Parse(filepath.Join(incidentsPath, entry.Name()))
 		if err != nil {
 			panic(err)
 
 		}
 
-		if !i.ShouldExclude(incident) {
-			importantIncidents = append(importantIncidents, incident)
+		if i.ShouldExclude(incident) {
+			slog.Debug("found: ", "name", name, "skip reason", "unimportant")
+			continue
 		}
+
+		slog.Info("found: ", "name", name)
+		importantIncidents = append(importantIncidents, incident)
 	}
 
+	slog.Info("data files found", "count", counter)
+	slog.Info("important incidents found", "count", len(importantIncidents))
+
+	// Init reporter
 	r, err := reporter.NewReporter(conf.TemplatePath, conf.ReportFilename)
 	if err != nil {
 		panic(err)
 	}
 
+	// Make report
 	r.Report(importantIncidents)
 
-}
-
-func printIncident(inc model.Incident) {
-	fmt.Println("Incident:")
-	fmt.Println("  ID:", inc.IncidentID)
-	fmt.Println("  Type:", inc.TargetType)
-	fmt.Println("  Name:", inc.TargetName)
-	fmt.Println("  Host:", inc.Host)
-	fmt.Println("  Time:", inc.IncidentCreationTime)
-	fmt.Println("  Message:", inc.Message)
 }
